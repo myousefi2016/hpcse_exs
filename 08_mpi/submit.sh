51bfb75361af20776127da219f59c7ed1e5ec349
superstar54 <xingwang1991@gmail.com>
2016-11-19 12:38:14 +0100

    myexs

--- 08_mpi/submit.sh
+++ 08_mpi/submit.sh
@@ -1,4 +1,4 @@
 #bsub -n 4 -o rank_size.out mpirun ./rank_size
-bsub -n 3 -o hello_world.out mpirun ./hello_world
+bsub -n 4 -o hello_world.out mpirun ./hello_world
 #bsub -n 2 -o mp_int.out mpirun ./mp_int
-bsub -n 2 -o deadblock5.out mpirun ./deadblock5
+#bsub -n 2 -o deadblock5.out mpirun ./deadblock5
