36b4e7d0728065ca57a50170e0258548eda7fa97
superstar54 <xingwang1991@gmail.com>
2016-11-21 23:06:06 +0800

    myexs

--- 08_mpi/diffusion1d.cpp
+++ 08_mpi/diffusion1d.cpp
@@ -1,17 +1,38 @@
 #include <mpi.h>
 #include <iostream>
+#include <cmath>
 
 int main(int argc, char const *argv[])
 {
-	double a = 0;
-	double b = 4;
+	double xs = 0;
+	double xe = 4;
 	double dx = 0.001;
+
 	double ts = 0;
 	double te = 10;
 	double dt = 0.001;
+
 	int nt = (te - ts)/dt;
 	int nx = (b - a)/dx;
-	
+
+	double density[nx];
+	// initialize the density
+	for(i=0; i<nx; i++) {
+		density[i] = std::sin(xs + i*dx)
+	}
+
+	for(i=0; i < nt; i++) {
+		for(j=1; j < nx - 1; j++) {
+			density[j] = (density[j+1] + density[j-1] - 2*density[i])/dx/dx*dt 
+			             + density[i]
+		}
+		density[0] = (density[2] + density[0] - 2*density[1])/dx/dx*dt 
+			             + density[0]
+		density[nx - 1] = (density[nx - 1] + density[nx - 3] - 2*density[nx - 2])/dx/dx*dt 
+			             + density[nx - 1]
+	}
+
+
 	MPI_Init(&argc, &argv);
 	MPI_Status status;
 	int rank;
