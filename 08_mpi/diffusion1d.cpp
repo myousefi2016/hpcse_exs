6fd4c0980047eae553caf4e015be5ed83198f306
superstar54 <xingwang1991@gmail.com>
2016-11-21 16:29:27 +0800

    myexs

--- 08_mpi/diffusion1d.cpp
+++ 08_mpi/diffusion1d.cpp
@@ -3,10 +3,21 @@
 
 int main(int argc, char const *argv[])
 {
+	double a = 0;
+	double b = 4;
+	double dx = 0.001;
+	double ts = 0;
+	double te = 10;
+	double dt = 0.001;
+	int nt = (te - ts)/dt;
+	int nx = (b - a)/dx;
+	
 	MPI_Init(&argc, &argv);
 	MPI_Status status;
 	int rank;
+	int size;
 	MPI_Comm_rank(&rank);
+	MPI_Comm_size(&size);
 
 	if(rank % 2 ==0) {
 		MPI_Send(&density[1])
