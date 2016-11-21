ea243789339478f2e5adc5068ee17746969b5141
superstar54 <xingwang1991@gmail.com>
2016-11-21 23:18:33 +0800

    myexs

--- 08_mpi/diffusion1d.cpp
+++ 08_mpi/diffusion1d.cpp
@@ -19,18 +19,18 @@ int main(int argc, char const *argv[])
 	double density[nx];
 	// initialize the density
 	for(int i=0; i<nx; i++) {
-		density[i] = std::sin(xs + i*dx)
+		density[i] = std::sin(xs + i*dx);
 	}
 
 	for(int i=0; i < nt; i++) {
 		for(int j=1; j < nx - 1; j++) {
 			density[j] = (density[j+1] + density[j-1] - 2*density[i])/dx/dx*dt 
-			             + density[i]
+			             + density[i];
 		}
 		density[0] = (density[2] + density[0] - 2*density[1])/dx/dx*dt 
-			             + density[0]
+			             + density[0];
 		density[nx - 1] = (density[nx - 1] + density[nx - 3] - 2*density[nx - 2])/dx/dx*dt 
-			             + density[nx - 1]
+			             + density[nx - 1];
 	}
 	ofstream dfile;
 	dfile.open("density.dat");
