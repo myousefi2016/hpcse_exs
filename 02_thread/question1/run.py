cca19cac5f485c86e95e0f8aabf401442cf57ee7
superstar54 <xingwang1991@gmail.com>
2016-12-08 12:11:03 +0100

    myexs

--- 02_thread/question1/run.py
+++ 02_thread/question1/run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 
 D=1
@@ -11,6 +10,6 @@ T=[1, 0.1, 0.001]
 Ns = [128, 256, 1024]
 dt = [0.00001, 0.000001, 0.00000001]
 for i in range(3):
-    os.system('./diffusion2d {0} {1} {2} {3} {4} {5} {6}' \
+    os.system('src/diffusion2d {0} {1} {2} {3} {4} {5} {6}' \
         .format(D, L, Ns[i], T[i], dt[i], Np, Nthreads) )
 
--- 02_thread/question1/run_cores.py
+++ 02_thread/question1/run_cores.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 
-cores = [1, 2, 4, 8, 12, 24]
+cores = [1, 2, 4, 8, 12, 16, 24]
 D=1
 L=2
 Np = 10000
 Nthreads = 4
-T=0.1
-Ns = 512
+T=0.01
+Ns = 1024 
 dt = 0.00001
 for Nthreads in cores:
-    os.system('./diffusion2d {0} {1} {2} {3} {4} {5} {6}' \
+    os.system('src/diffusion2d_thread {0} {1} {2} {3} {4} {5} {6}' \
         .format(D, L, Ns, T, dt, Np, Nthreads) )
 
