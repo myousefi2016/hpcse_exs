2ac906cba53c8151c7a94e519ac7ce842fc9cf25
superstar54 <xingwang1991@gmail.com>
2016-12-08 15:30:46 +0800

    myexs

--- 02_thread/question1/diffusion2d.cpp
+++ 02_thread/question1/diffusion2d.cpp
@@ -1,60 +1,93 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 #include <fstream>
 #include "timer.hpp"
+
+
+void write_rho(std::vector<double> rho, int N, double L, double dr, std::string filename)
+{
+	std::ofstream myfile;
+	myfile.open(filename);
+	for(int i=0; i<N; i++) {
+		for(int j=0; j<N; j++) {
+			myfile << i*dr - L/2 << "\t" << j*dr -L/2 <<"\t" << rho[i*N+j] << std::endl;
+		}
+	}
+}
 
 int main(int argc, char const *argv[])
 {
 
 	//input
+	double L = 2;
 	double D; // diffusion coefficient
 	int N;  //
+	double T;
 	double dt;
+	std::vector<double> rho;
+	std::vector<double> rho_new;
+
 	if(argc<5) {
 		std::cout << "Please input D, N, t, dt" <<std::endl;
 		return 1;
 	}
 	else {
-		D = argv[1];
-		N = argv[2];
-		T = argv[3];
-		dt = argv[4];
+		D = std::stod(argv[1]);
+		N = std::stod(argv[2]);
+		T = std::stod(argv[3]);
+		dt = std::stod(argv[4]);
 	}
-	double coef = D*dt/N/N;
-	double rho[N*N]; //
-	double dx = 1.0/N; //
+	double dr = L/(N-1); // important!!! N-1 not N
+	double coef = D*dt/dr/dr;
+	rho.resize(N*N);
+	rho_new.resize(N*N);
 	//initial
-	for(int i=1; i<N/2; i++) {
-		for(int j=0; j<N/2; j++) {
-			rho[i*N + j] = 1;
-		}
-	}
-	for(int i=N/2; i<N; i++) {
-		for(int j=N/2; j<N; j++) {
-			rho[i*N + j] = 0;
-		}
-	}
-	double rho_new[N*N];
-	rho_new = rho;
-
-	//forward Euler and central difference
-	double t = 0
-	while(t<T) {
-		for(int i=1; i<N-1;i++) {
-			for(int j=0;j<N;j++) {
-				rho_new[i*N + j] = rho[i*N + j] + coef*(
-					rho[(i+1)*N + j] + rho[(i-1)*N + j] - 2rho[(i)*N + j]
-				  + rho[(i)*N + j+1] + rho[(i)*N + j-1] - 2rho[(i)*N + j])
+	double boud = 1.0/2;
+	for(int i=0; i<N; ++i) {
+		for(int j=0; j<N; ++j) {
+			if(std::abs(i*dr-L/2)<boud && std::abs(j*dr-L/2)<boud) {
+				rho[i*N + j] = 1;
+			}
+			else {
+				rho[i*N + j] = 0;
 			}
 		}
 	}
+	write_rho(rho, N, L, dr, "input.dat");
 
-	//output
-	std::fstream myfile;
-	myfile.open('output.dat');
-	for(int i=0; i<N; i++) {
-		for(int j=0; j<N; j++) {
-			myfile << i << "\t" << j <<"\t" << rho[i*N+j] << std::endl;
+	// rho_new = rho;
+
+	//forward Euler and central difference
+	double t = 0;
+	int k = 0;
+	std::string filename;
+	while(t<T) {
+		for(int i=0; i<N-1;++i) {
+			for(int j=0;j<N-1;++j) {
+				rho_new[i*N + j] = rho[i*N + j] + coef*(
+					(i==N-1?0:rho[(i+1)*N + j]) + 
+					(i==0?0:rho[(i-1)*N + j]) + 
+					(j==N-1?0:rho[(i)*N + j+1]) + 
+				 	(j==0?0:rho[(i)*N + j-1]) - 
+				    4*rho[(i)*N + j]);
+			}
 		}
+
+		t += dt;
+		std::swap(rho, rho_new);
+		// for(int i=1; i<N-1;i++) {
+		// 	for(int j=0;j<N;j++) {
+		// 		rho[i*N+j] = rho_new[i*N+j];			}
+		// }
+		if(k%100==0) {
+			filename = "output_" + std::to_string(k) + ".dat";
+			write_rho(rho, N, L, dr, filename);
+		}
+		k++;
 	}
+	//output
+	// write_rho(rho, N, L, dr, "output.dat");
 	return 0;
 }
---
+++ 02_thread/question1/plot_rho.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+files = [['density_000.dat', 'density_025.dat', 'density_050.dat', 'density_075.dat'],
+         ['density_100.dat', 'density_125.dat', 'density_150.dat', 'density_175.dat']]
+
+times = [[0.00, 0.25, 0.50, 0.75],
+         [1.00, 1.25, 1.50, 1.75]]
+
+N = 50
+
+fig = plt.figure()
+
+for i in range(8):
+    fname = "output_{0}.dat".format(i*1000)
+    data = np.loadtxt(fname)
+    X = data[:,0].reshape((N,N))
+    Y = data[:,1].reshape((N,N))
+    VAL = data[:,2].reshape((N,N))
+    
+    ax = plt.subplot(2,4,i+1)
+    im = plt.imshow(VAL, vmin=0, vmax=0.1, extent=(-1,1,-1,1))
+    
+   
+    plt.xticks([-1,0,1])
+    plt.yticks([-1,0,1])
+    
+    if i == 0:
+        plt.xticks([])
+
+
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+fig.colorbar(im, cax=cbar_ax)
+
+plt.savefig('densitiy_barrier.pdf')
+plt.show()
